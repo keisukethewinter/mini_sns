230e7d1cc16ec1a25d49fef2cf3e80f1aaed457a
keisukethewinter <addict20fx1@gmail.com>
2020-09-29 19:58:00 +0900

    Update user.rb
    Userモデルに入るデータなので、Userモデルにクラスメソッドを作成.
    omniauth_callbacks_controller.rbに記述した、User.from_omniauthも呼び出せるようになった。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+  end
 end
