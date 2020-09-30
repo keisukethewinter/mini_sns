4bc337c62e42f7665a92502b859fdf32a23ce7bd
keisukethewinter <addict20fx1@gmail.com>
2020-10-01 01:15:28 +0900

    user modelを編集
    persisted?で登録済みのユーザーと判断を行い、「if文」の中が呼ばれます。新規登録時は、SnsCredentialモデルに保存されるタイミングで、user_idが確定していなかったので、SnsCredentialモデルとUserモデルは紐付いていません。ログインの際に、sns.userを更新して紐付けを行います。

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,10 @@ class User < ApplicationRecord
     nickname: auth.info.name,
     email: auth.info.email
   )
+  # userが登録済みであるか判断
+  if user.persisted?
+    sns.user = user
+    sns.save
   end
+  user
 end
